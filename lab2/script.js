47aa0b96e0232ac312e52161ee5ec5386ecb92a7
Карпенко Глеб <glebkarpenko1@gmail.com>
2024-04-29 13:47:03 +0300

    add teacher info popup

--- lab2/script.js
+++ lab2/script.js
@@ -2,8 +2,10 @@
 const leftButton = document.getElementById("slide-left");
 const rightButton = document.getElementById("slide-right");
 const itemSlider = document.getElementById("item-slider");
+let addTeacherControllers = document.querySelectorAll(".add-teacher-controller");
+let teacherInfoControllers = document.querySelectorAll(".teacher-info-controller");
+let imageWrappers = document.querySelectorAll(".imageWrapper");
 
-let popupControllerButtons = document.querySelectorAll(".popup-controller");
 let hoverTimer;
 
 leftButton.addEventListener('click', () => {
@@ -20,12 +22,26 @@ rightButton.addEventListener('click', () => {
     });
 });
 
-popupControllerButtons.forEach((item) => {
-    item.addEventListener('click', () => {
-        document.querySelector('.popup').classList.toggle('active');
-        document.getElementById('page-container').classList.toggle('blured');
+addTeacherControllers.forEach((item) => {
+    item.addEventListener('click', () => togglePopup('add-teacher'));
+});
+
+teacherInfoControllers.forEach((item) => {
+    item.addEventListener('click', () => togglePopup('teacher-info'));
+});
+
+imageWrappers.forEach((item) => {
+    item.addEventListener('click', function(event) {
+        if (event.target.classList.contains('teacher-info-controller')) {
+            togglePopup('teacher-info');
+        }
     });
-})
+});
+
+function togglePopup(popupID){
+    document.getElementById(popupID).classList.toggle('active');
+    document.getElementById('page-container').classList.toggle('blured');
+}
 
 // Change the src attribute to the hover image with timed delay and animation
 function changeLogo(imgSrc) {
