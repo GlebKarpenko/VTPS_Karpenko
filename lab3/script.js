f1516538a28406380dfbca222d2059dfd73ca471
Карпенко Глеб <glebkarpenko1@gmail.com>
2024-05-01 16:32:05 +0300

    update lab3

--- lab3/script.js
+++ lab3/script.js
@@ -1,110 +1,70 @@
-const randomUserMock = {
-    "gender": "male",
-    "title": "Mr",
-    "full_name": "Norbert Weishaupt",
-    "city": "Rhön-Grabfeld",
-    "state": "Mecklenburg-Vorpommern",
-    "country": "Germany",
-    "postcode": 52640,
-    "coordinates": { "latitude": "-42.1817", "longitude": "-152.1685" },
-    "timezone": { "offset": "+9:30", "description": "Adelaide, Darwin" },
-    "email": "norbert.weishaupt@example.com",
-    "b_date": "1956-12-23T19:09:19.602Z",
-    "age": 65,
-    "phone": "0079-8291509",
-    "picture_large": "https://randomuser.me/api/portraits/men/28.jpg",
-    "picture_thumbnail": "https://randomuser.me/api/portraits/thumb/men/28.jpg"
-}
-
-const additionalUsers = {
-    "gender": "male",
-    "title": "Mr",
-    "full_name": "Norbert Weishaupt",
-    "city": "Rhön-Grabfeld",
-    "state": "Mecklenburg-Vorpommern",
-    "country": "Germany",
-    "postcode": 52640,
-    "coordinates": { "latitude": "-42.1817", "longitude": "-152.1685" },
-    "timezone": { "offset": "+9:30", "description": "Adelaide, Darwin" },
-    "email": "norbert.weishaupt@example.com",
-    "b_date": "1956-12-23T19:09:19.602Z",
-    "age": 65,
-    "phone": "0079-8291509",
-    "picture_large": "https://randomuser.me/api/portraits/men/28.jpg",
-    "picture_thumbnail": "https://randomuser.me/api/portraits/thumb/men/28.jpg",
-    "education": "Ph.D. in Computer Science",
-    "hobbies": ["Reading", "Photography", "Hiking"]
-}
-
-const randomUserMock2 = {
-    "gender": "male",
-    "full_name": "Richard Roussel",
-    "city": "Mettmenstetten",
-    "state": "Aargau",
-    "country": "Switzerland",
-    "postcode": 3918,
-    "coordinates": {
-    "latitude": "-63.6075",
-    "longitude": "-97.4008"
-    },
-    "timezone": {
-    "offset": "-7:00",
-    "description": "Mountain Time (US & Canada)"
-    },
-    "email": "richard.roussel@example.com",
-    "login": {
-    "uuid": "de3be804-d0e7-44a3-b815-b93b53bc4579",
-    "username": "beautifulgoose727",
-    "password": "lalakers",
-    "salt": "Zd3BpQn8",
-    "md5": "bb751a56fc1b0f14e2aeb51cccb1e88b",
-    "sha1": "cdd562fea4c1e78098410bc6c1c74c639c944ddf",
-    "sha256": "122ecd01d4e73f6f383772468f716a095962a1dea2ecc1609976c2065b270592"
-    },
-    "dob": "1979-09-11T22:33:39.696Z",
-    "age": 44,
-    "registered": {
-    "date": "2006-10-15T19:42:25.460Z",
-    "age": 17
-    },
-    "phone": "075 772 34 61",
-    "cell": "075 637 97 85",
-    "id": {
-    "name": "AVS",
-    "value": "756.9716.2620.37"
-    },
-    "picture": {
-    "large": "https://randomuser.me/api/portraits/men/70.jpg",
-    "medium": "https://randomuser.me/api/portraits/med/men/70.jpg",
-    "thumbnail": "https://randomuser.me/api/portraits/thumb/men/70.jpg"
-    },
-    "nat": "CH"
-}
+import { randomUserMock, additionalUsers } from "./FE4U-Lab3-mock.js";
 
 //
 //----------------Task 1----------------
 //
 
-function formatObjects(objects = []){
+function addRequiredFields(objectArray) {
+    const requiredFields = ["full_name", "gender", "note", "state", "city", "country", "favorite"];
+
+    for (const obj of objectArray) {
+        for (const field of requiredFields) {
+            if (!(field in obj)) {
+                obj[field] = null;
+            }
+        }
+    }
+
+    return objectArray;
+}
+
+function formatUserData(objects = []) {
     const courses = ["Mathematics", "Physics", "English", "Computer Science", "Dancing", "Chess", "Biology", "Chemistry", "Law", "Art", "Medicine", "Statistics"];
     let id = 0;
     
     objects.forEach(object => {
-        id++;
-        object.id = id;
-        object.favourite = new Boolean(false);
-        const randomIndex = Math.floor(Math.random() * courses.length);
-        object.course = courses[randomIndex];
-        bg_color = "#f9f5f9";
+        try {
+            id++;
+            object.id = id;
+            const randomIndex = Math.floor(Math.random() * courses.length);
+            object.course = courses[randomIndex];
+            object.full_name ??= `${object.name.first} ${object.name.last}`;
+            object.gender ??= "None";
+            object.gender = converGenderToStandart(object.gender);     
+            object.note ??= `This is ${object.course} teacher.`;
+            object.state ??= object.location.state;
+            object.city ??= object.location.city;
+            object.country ??= object.location.country;
+            object.favorite ??= new Boolean(false);
+            object.bg_color = "#f9f5f9";
+        } catch(error) {
+            console.log("Could not format this user! Not enough data.", error);
+        }
     });
 
-    objects.push({ ...randomUserMock, ...additionalUsers});
     return objects;
 }
 
+function converGenderToStandart(gender) {
+    const male = "Male";
+    const female = "Female";
+    const maleRegex = /^(m|male)$/i;
+    const femaleRegex = /^(f|female)$/i;
+
+    if (maleRegex.test(gender)) {
+        return male;
+    }
+    if (femaleRegex.test(gender)) {
+        return female
+    }
+    return "None";
+}
+
 console.log("\nTask1");
-console.log("formated objects: ");
-console.log(formatObjects([randomUserMock, additionalUsers, randomUserMock]));
+console.log("Formated objects: ");
+let mockObjects = [...randomUserMock, ...additionalUsers]
+mockObjects = addRequiredFields(mockObjects);
+mockObjects = formatUserData(mockObjects);
 
 //
 //----------------Task 2----------------
@@ -113,31 +73,37 @@ console.log(formatObjects([randomUserMock, additionalUsers, randomUserMock]));
 function userIsValid(user) {
     const requiredFields = ["full_name", "gender", "note", "state", "city", "country"];
     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    const phoneRegex = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+    const phoneRegex = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/g;
 
-    for (let field of requiredFields) {
-        if (typeof user[field] !== 'string' || 
-        user[field].charAt(0) !== user[field].charAt(0).toUpperCase()) {
+    try {
+        for (let field of requiredFields) {
+            if (typeof user[field] !== 'string'  
+            || user[field].charAt(0) !== user[field].charAt(0).toUpperCase()) {
+                return false;
+            }
+        }
+    
+        if (typeof user.dob.age !== "number") {
             return false;
         }
-    }
+    
+        if (!phoneRegex.test(user.phone)){
+            return false
+        }
+    
+        if (!emailRegex.test(user.email)) {
+            return false;
+        }
 
-    if (typeof user.age !== "number") {
-        return false;
+        return true;
+    } catch (error) {
+        console.log(`User with id: ${user.id} is not valid! `, error);
     }
-
-    if (!phoneRegex.test(user.phone)){
-        return false
-    }
-
-    if (!emailRegex.test(user.email)) {
-        return false;
-    }
-    return true
+    return false;
 }
 
 console.log("\nTask2");
-console.log(`User with name ${randomUserMock.full_name} is valid: ${userIsValid(randomUserMock)}`);
+console.log(`User with name ${mockObjects[6].full_name} is valid: ${userIsValid(mockObjects)}`);
 
 //
 //----------------Task 3----------------
@@ -154,20 +120,19 @@ function filterUsers(users, filterFields) {
     })
 }
 
-const usersToFilter = [
-    randomUserMock,
-    { ...randomUserMock, favourite: true }
-]
+const usersToFilter = mockObjects;
 
 const filterFields = {
     "country": "Germany",
-    "age": 65,
-    "gender": "male",
-    "favourite": true
+    "gender": "Male",
 }
 
 console.log("\nTask3");
 console.log("Filtered users: ", filterUsers(usersToFilter, filterFields));
+
+//
+//----------------Task 4----------------
+//
 
 function sortUsers(users, sortBy, order="desc") {
     const validFields = ["full_name", "age", "b_day", "country"];
@@ -197,14 +162,10 @@ function sortUsers(users, sortBy, order="desc") {
 }
 
 const sortBy = "age";
-const usersToSort = [
-    randomUserMock,
-    additionalUsers,
-    randomUserMock2
-];
+const usersToSort = mockObjects;
 
 console.log("\nTask4");
-console.log(`Users sorted by "${sortBy}" field:`, sortUsers(usersToSort, sortBy, order='asc'));
+console.log(`Users sorted by "${sortBy}" field:`, sortUsers(usersToSort, sortBy));
 
 //
 //----------------Task 5----------------
@@ -227,11 +188,7 @@ function findFirst(users, searchBy){
     return result;
 }
 
-const usersToSearchIn = [
-    randomUserMock,
-    additionalUsers,
-    randomUserMock2
-]
+const usersToSearchIn = mockObjects;
 
 const searchBy = {
     "age": 65
